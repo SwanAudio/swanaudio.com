eea70a3c8691a71c77c80032c1dd80ef044e7b94
Evan Lowry <lowry.e@gmail.com>
2014-04-07 22:18:02 -0300

    Set swappable images

--- app/assets/javascript/products.js
+++ app/assets/javascript/products.js
@@ -4,21 +4,17 @@ var app = angular.module('swanaudio', []);
 
 app.controller('img-swap', ['$scope', '$http', 
   function ($scope, $http) { 
-    $scope.images = [{
-      id: 1,
-      label: '',
-      url: ''
-    },{
-      id: 2,
-      label: '',
-      url: ''
-    }];
-
     $http.get('product/list').success(function(data) {
-      $scope.products = data
+      $scope.products = data;
     });
 
-    $scope.$watch('', function(id) {
+    $http.get('product/detail/1').success(function(data) {
+      $scope.product = data;
+      $scope.active_image = data.images[0];
     });
+
+    $scope.swap_image = function(img) {
+      $scope.active_image = img;
+    };
   }
 ]);
--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -6,42 +6,54 @@ import play.api.libs.json._
 
 object Products extends Controller {
 
-    def list = Action {
-        val json = Json.arr(
-            Json.obj(
-                "id" -> 1,
-                "name" -> "Product 1"
-            ),
-            Json.obj(
-                "id" -> 2,
-                "name" -> "Product 2"
-            )
-        )
-        Ok(json)
-    }
+  def list = Action {
+    val json = Json.arr(
+      Json.obj(
+        "id" -> 1,
+        "name" -> "Product 1"
+      ),
+      Json.obj(
+        "id" -> 2,
+        "name" -> "Product 2"
+      )
+    )
 
-    def detail(id: Long) = Action {
-        // Retreive based on ID
-        // Temp: Static object
-        val json = Json.obj(
-            "id" -> 1,
-            "name" -> "Product 1",
-            "main-images" -> Json.arr(
-                Json.obj(
-                    "url" -> "tmp",
-                    "label" -> "tmp",
-                    "alt" -> "tmp"
-                )
-            ),
-            "aux-images" -> Json.arr(
-                Json.obj(
-                    "url" -> "tmp",
-                    "label" -> "tmp",
-                    "alt" -> "tmp"
-                )
-            ),
-            "details" -> "todo"
+    Ok(json)
+  }
+
+  def detail(id: Long) = Action {
+    // Retreive based on ID
+    // Temp: Static object
+    val json = Json.obj(
+      "id" -> 1,
+      "name" -> "Product 1",
+      "images" -> Json.arr(
+        Json.obj(
+          "url" -> "http://placehold.it/800x400",
+          "label" -> "tmp",
+          "alt" -> "tmp"
+        ),
+        Json.obj(
+          "url" -> "http://placehold.it/100x100",
+          "label" -> "tmp",
+          "alt" -> "tmp"
+        ),
+        Json.obj(
+          "url" -> "http://placehold.it/200x100",
+          "label" -> "tmp",
+          "alt" -> "tmp"
         )
-        Ok(json)
-    }
+      ),
+      "graphs" -> Json.arr(
+        Json.obj(
+          "url" -> "tmp",
+          "label" -> "tmp",
+          "alt" -> "tmp"
+        )
+      ),
+      "details" -> "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+
+    Ok(json)
+  }
 }
