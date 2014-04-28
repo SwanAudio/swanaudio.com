75d80fd2a82a07b4ed8f8df9b8b34d536013616d
Evan Lowry <lowry.e@gmail.com>
2014-04-27 21:38:29 -0300

    Update to play2.3

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -44,6 +44,35 @@ object Products extends Controller {
           "alt" -> "tmp"
         )
       ),
+      "options" -> Json.arr(
+        Json.obj(
+          "type" -> "drop",
+          "label" -> "Cable Length",
+          "options" -> Json.arr(
+            Json.obj(
+              "1" -> "1 Meter",
+              "2" -> "2 Meter",
+              "3" -> "3 Meter"
+            )
+          )
+        )
+      ),
+      "specs" -> Json.arr(
+        Json.obj(
+          "Capacitance @ 100Hz" -> "75pF +/- 5pF",
+          "Capacitance @ 10kHz" -> "85pF +/- 10pF",
+          "3dB Low Frequency" -> "24Hz +/- 2Hz",
+          "3dB High Frequency" -> "111kHz +/- 10kHz",
+          "Conductor ohms / m" -> "0.5 ohm +/ 0.1ohm",
+          "Shield ohms / m" -> "0.8 ohm +/ 0.1ohm",
+          "Bend Radius" -> "0.25 m",
+          "Conductor Size" -> "22 AWG",
+          "Shield Size" -> "22 AWG",
+          "Primary Shield Type" -> "Double Reussen Copper Weave",
+          "Secondary Shield Type" -> "Radiated Carbon Jacket",
+          "Cable Outer Jacket" -> "Black PVC"
+        )
+      ),
       "graphs" -> Json.arr(
         Json.obj(
           "url" -> "tmp",
--- build.sbt
+++ build.sbt
@@ -2,15 +2,14 @@ name := "swanaudio"
 
 version := "1.0-SNAPSHOT"
 
+lazy val root = (project in file(".")).addPlugins(PlayScala)
+
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache,
-  "org.webjars" %% "webjars-play" % "2.2.0",
+  "org.webjars" %% "webjars-play" % "2.3-M1",
   "org.webjars" % "bootstrap" % "3.1.0",
   "org.webjars" % "angularjs" % "1.3.0-beta.2"
 )
 
 play.Project.playScalaSettings
 
-com.jamesward.play.BrowserNotifierPlugin.livereload
+//com.jamesward.play.BrowserNotifierPlugin.livereload
