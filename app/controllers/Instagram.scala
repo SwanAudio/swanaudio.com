9955e8fa449b488e64a0304393d229df71b2c549
Evan Lowry <lowry.e@gmail.com>
2018-01-01 22:11:31 -0400

    Re-enable caching on instagram route
    We don't post on instagram that much, no need to go external for every
    page hit.

--- app/controllers/Instagram.scala
+++ app/controllers/Instagram.scala
@@ -19,7 +19,7 @@ class Instagram @Inject() (
 
   val recentUrl = "https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables=%7B%22id%22%3A%224514254001%22%2C%22first%22%3A10%2C%22after%22%3Anull%7D"
 
-  def recent = //cached(_ => "instagram-recent", duration=900) {
+  def recent = cached(_ => "instagram-recent", duration=900) {
     Action.async { implicit request =>
       ws.url(recentUrl)
         .addHttpHeaders("Accept" -> "application/json")
@@ -35,6 +35,6 @@ class Instagram @Inject() (
         )
         .map(json => Ok(json.noSpaces))
     }
-  //}
+  }
 
 }
